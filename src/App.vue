7736db5e27284843b90de8bdfbcbeeb2ef640fa2
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-10-24 22:13:14 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Form />
+</template>
+
+<script>
+import Form from "./components/Form.vue";
+export default {
+  name: "App",
+  components: { Form },
+};
+</script>
+
+<style></style>
---
+++ src/components/Form.vue
@@ -0,0 +1,14 @@
+<template>
+  <form @submit.prevent="">
+    <label for="">Title:<input type="text"/></label><br />
+    <label>Description:<input type="text"/></label><br />
+    <label>Date:<input type="date"/></label></form
+></template>
+
+<script>
+export default {
+  name: "Form",
+};
+</script>
+
+<style></style>
