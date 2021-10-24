17b17b22fb0700668376c3194710703adbae728d
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-10-24 22:29:25 +0200

    Configure form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,14 +1,39 @@
 <template>
-  <form @submit.prevent="">
-    <label for="">Title:<input type="text"/></label><br />
-    <label>Description:<input type="text"/></label><br />
-    <label>Date:<input type="date"/></label></form
+  <div>
+    <form @submit.prevent="">
+      <div class="wrapper">
+        <label>Title:<input type="text" v-model="title"/></label><br />
+        <label>Description:<textarea v-model="description"/></label><br />
+        <label>Date:<input type="date" v-model="date"/></label>
+      </div>
+    </form></div
 ></template>
 
 <script>
 export default {
   name: "Form",
+  data: () => {
+    return {
+      title: "",
+      description: "",
+      date: "",
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.wrapper {
+  margin: 100px auto;
+}
+form div {
+  width: 20%;
+  border: 2px solid black;
+  padding: 10px;
+  font-family: sans-serif;
+}
+label {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
